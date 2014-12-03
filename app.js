3e13f4418b942fcd16cc6382d335b47fb29cfeaf
Алексей Новиков <anovikov@wezzet.com>
2014-12-03 23:11:56 +0300

    Добавлен вывод корректных ссылок

--- app.js
+++ app.js
@@ -32,10 +32,6 @@ var rli = readline.createInterface({
     output  : process.stdout
 });
 
-
-Manager.events.emit('notification:hip-chat:buildComplete', '0.0.0.0', 'TEST');
-return;
-
 async.series([
     Updater.checkUpdate.bind(Updater, rli),
     Checker.config.checkFile.bind(Checker.config, rli),
@@ -48,7 +44,7 @@ async.series([
     }
 ], function() {
 
-    var project = version = versionId = prevCommand = pckVersion = folder = jiraNameVersion = jiraProjectName = null,
+    var project = version = versionId = prevCommand = pckVersion = folder = jiraNameVersion = jiraProjectName = jiraProjectKey = null,
         issuesVersion = [],
         flag    = step2 = installFlag = stopNode = false,
         jira    = new JiraApi(config.service.jira.protocol, config.service.jira.host, config.service.jira.port, config.service.jira.user, config.service.jira.password, config.service.jira.api);
@@ -109,6 +105,7 @@ async.series([
 
                     jiraNameVersion = config.projects[project].jira['version-name'].replace('{version}', version);
 					jiraProjectName = projectJira.name;
+					jiraProjectKey 	= projectJira.key;
 
                     var versionJira = _.findWhere(projectJira.versions, {
                         name : jiraNameVersion
@@ -425,7 +422,8 @@ async.series([
                     }
                 },
                 onEnd: function(sessionText, sshObj) {
-					Manager.events.emit('notification:hip-chat:buildComplete', jiraNameVersion, jiraProjectName);
+					// Arguments: jiraVersionId, jiraProjectKey, jiraVersionName, jiraProjectName
+					Manager.events.emit('notification:hip-chat:buildComplete', versionId, jiraProjectKey, jiraNameVersion, jiraProjectName);
 
                     fs.writeFile([__dirname, folder + '.log'].join('/'), sessionText, function(error) {
                         if(error) {
